995bfcc73e5e65558c79ff434018f5d6f6ca6deb
Manojay13 <electraatoma@gmail.com>
2026-08-05 00:12:14 +0530

    14-Pointers/01-Variables/01-PointerVariable/02-DeclarationMethod_02/04-Char/PointerVariable_Char.c Assignment Completed

--- 01-CAssignments/14-Pointers/01-Variables/01-PointerVariable/02-DeclarationMethod_02/04-Char/PointerVariable_Char.c
+++ 01-CAssignments/14-Pointers/01-Variables/01-PointerVariable/02-DeclarationMethod_02/04-Char/PointerVariable_Char.c
@@ -4,11 +4,25 @@ int main(void)
 {
 	//variable declarations
 	char ch_mk;
-	char* ptr = NULL; //Declaration Method 2 :- 'ptr_mk' is a variable of type 'char*' 	
+	char* ptr_mk = NULL; //Declaration Method 2 :- 'ptr_mk' is a variable of type 'char*' 	
 	//code
 	ch_mk = 'A';
 
 	printf(" *****BEFORE ptr_mk = &ch_mk *****\n\n");
-	printf("Value of 'ch_mk		=%d\n\n", ch_mk);
-	printf("")
+	printf("Value of 'ch_mk			=%d\n\n", ch_mk);
+	printf("Address of 'ch_mk'		=%p\n\n", &ch_mk);
+	printf("Value at Address Of 'ch_mk' 	=%c\n\n", *(&ch_mk));
+
+	//Assigning address of variable 'ch_mk' to pointer 'ptr_mk'
+	//'ptr_mk' now contains address of 'ch_mk' ...hence ,'ptr_mk' is SAME as '&ch_mk'
+	ptr_mk = &ch_mk;
+
+	printf("\n\n");
+
+	printf(" ***** AFTER ptr_mk = &ch_mk *****\n\n");
+	printf("Value of 'ch_mk'		=%c\n\n",ch_mk);
+	printf("Address Of 'ch_mk'		=%p\n\n", ptr_mk);
+	printf("Value  At Address Of 'ch_mk' 	=%c\n\n",*ptr_mk);
+
+	return(0);
 }
