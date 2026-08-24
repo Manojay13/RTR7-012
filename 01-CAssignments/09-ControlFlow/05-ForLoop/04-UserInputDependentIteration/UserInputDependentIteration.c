dc147fd8d89804a21a960b74c04516a066f4c418
Manojay13 <electraatoma@gmail.com>
2026-08-24 07:27:04 +0530

    /09-ControlFlow/05-ForLoop/04-UserInputDependentIteration/ Assignment Added

---
+++ 01-CAssignments/09-ControlFlow/05-ForLoop/04-UserInputDependentIteration/UserInputDependentIteration.c
@@ -0,0 +1,26 @@
+#include<stdio.h>// for printf()
+#include<stdlib.h> // for exit()
+
+int main(void)
+{
+	//variable declarations
+	int i_num_mk, num_mk, i_mk;
+
+	//code
+	printf("\n\n");
+
+	printf("Enter An Integer Value From Which Iteration Must Begin : ");
+	scanf("%d", &i_num_mk);
+
+	printf("How Many Digits Do You Want To  From %d OnWards? : ", i_num_mk);
+	scanf("%d", &num_mk);
+
+	printf("Printing Digits %d to %d : \n\n", i_num_mk, (i_num_mk + num_mk));
+
+	for(i_mk = i_num_mk;i_mk <= (i_num_mk + num_mk); i_mk++)
+	{
+		printf("\t%d\n", i_mk);
+	}
+	printf("\n\n");
+	exit(EXIT_SUCCESS);
+}
