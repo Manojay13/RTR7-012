efe2f0911b74cb65e6eac144e548d381ad49f754
Manojay13 <electraatoma@gmail.com>
2026-08-26 10:13:32 +0530

    09-ControlFlow/07-DoWhileLoop/03-FloatingPointVariableIteration Assignemnt completed

---
+++ 01-CAssignments/09-ControlFlow/07-DoWhileLoop/03-FloatingPointVariableIteration/FloatingPointVariableIteration.c
@@ -0,0 +1,24 @@
+#include<stdio.h>//for printf()
+#include<stdlib.h>//for exit()
+int main(void)
+{
+	//variable declarations
+	float f_mk;
+	float f_num_mk = 1.7f; //simply change this value ONLY to get different outputs...
+
+	//code
+	printf("\n\n");
+
+	printf("Printing Numbers %f to %f : \n\n", f_num_mk, (f_num_mk * 10.0f));
+
+	f_mk = f_num_mk;
+	do
+	{
+		printf("\t%f\n", f_mk);
+		f_mk = f_mk + f_num_mk;
+	}while (f_mk <= (f_num_mk * 10.0f));
+
+	printf("\n\n");
+
+	exit(EXIT_SUCCESS);
+}
