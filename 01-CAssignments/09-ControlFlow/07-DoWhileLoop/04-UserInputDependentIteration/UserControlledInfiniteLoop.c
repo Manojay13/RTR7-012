722b90f20f674b3e5381800e94e8c39cbdc32b60
Manojay13 <electraatoma@gmail.com>
2026-08-26 14:22:00 +0530

    09-ControlFlow/07-DoWhileLoop/04-UserInputDependentIteration/ Assignemnt Completed

---
+++ 01-CAssignments/09-ControlFlow/07-DoWhileLoop/04-UserInputDependentIteration/UserControlledInfiniteLoop.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(void)
+{
+	//variable declarations
+	int i_num_mk, num_mk, i_mk;
+
+	//code
+	printf("\n\n");
+
+	printf("Enter An Integer Value From Which Iteration Must Begin : ");
+	scanf("%d", &i_num_mk);
+
+	printf("How Many Digits Do You Want To Print From %d Onwards ? : ", i_num_mk);
+	scanf("%d", &num_mk);
+
+	printf("Printing Digits %d to %d : \n\n", i_num_mk, (i_num_mk + num_mk));
+
+	i_mk = i_num_mk;
+	do
+	{
+		printf("\t%d\n", i_mk);
+		i_mk++;
+	}while (i_mk <= (i_num_mk + num_mk));
+
+	printf("\n\n");
+
+	return(0);
+}
