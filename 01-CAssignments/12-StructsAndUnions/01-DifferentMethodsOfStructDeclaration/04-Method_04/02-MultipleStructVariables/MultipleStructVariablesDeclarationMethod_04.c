2748e4f110fcf060e0d73ea1d061f0f763925cd3
Manojay13 <electraatoma@gmail.com>
2026-08-31 19:14:39 +0530

    01-CAssignments/12-StructsAndUnions/01-DifferentMethodsOfStructDeclaration/04-Method_04/02-MultipleStructVariables/ Assignment Completed

---
+++ 01-CAssignments/12-StructsAndUnions/01-DifferentMethodsOfStructDeclaration/04-Method_04/02-MultipleStructVariables/MultipleStructVariablesDeclarationMethod_04.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+//DEFINING STRUCT
+struct MyPoint_mk
+{
+	int x_mk;
+	int y_mk;
+};
+
+int main(void)
+{
+	//variable declarations
+	struct MyPoint_mk point_A_mk, point_B_mk, point_C_mk, point_D_mk, point_E_mk; //declaring 5 struct variables of type 'struct MyPoint_mk' locally...
+
+	//code
+	//Assigning Data Values To The Data Members Of 'struct MyPoint_mk' variable 'point_A_mk'
+	point_A_mk.x_mk = 3;
+	point_A_mk.y_mk = 0;
+
+	//Assigning Data Values To The Data Members Of 'struct MyPoint_mk' variable 'point_B_mk'
+	point_B_mk.x_mk = 1;
+	point_B_mk.y_mk = 2;
+
+	//Assigning Data Values To The Data Members Of 'struct MyPoint_mk' variable 'point_C_mk'
+	point_C_mk.x_mk = 9;
+	point_C_mk.y_mk = 6;
+
+	//Assigning Data Values To The Data Members Of 'struct MyPoint_mk' variable 'point_D_mk'
+	point_D_mk.x_mk = 8;
+	point_D_mk.y_mk = 2;
+
+	//Assigning Data Values To The Data Members Of 'struct MyPoint_mk' variable 'point_E_mk'
+	point_E_mk.x_mk = 11;
+	point_E_mk.y_mk = 8;
+
+	//Displaying Values Of The Data Members Of 'struct MyPoint_mk' (all variables)
+	printf("\n\n");
+	printf("Co-ordinates (x_mk, y_mk) Of Point 'A_mk' Are : (%d, %d)\n\n", point_A_mk.x_mk, point_A_mk.y_mk);
+	printf("Co-ordinates (x_mk, y_mk) Of Point 'B_mk' Are : (%d, %d)\n\n", point_B_mk.x_mk, point_B_mk.y_mk);
+	printf("Co-ordinates (x_mk, y_mk) Of Point 'C_mk' Are : (%d, %d)\n\n", point_C_mk.x_mk, point_C_mk.y_mk);
+	printf("Co-ordinates (x_mk, y_mk) Of Point 'D_mk' Are : (%d, %d)\n\n", point_D_mk.x_mk, point_D_mk.y_mk);
+	printf("Co-ordinates (x_mk, y_mk) Of Point 'E_mk' Are : (%d, %d)\n\n", point_E_mk.x_mk, point_E_mk.y_mk);
+
+	return(0);
+}
